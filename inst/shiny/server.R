e074c817be022b28bdc7de30cdb041aad08a4be4
Nathalie Villa-Vialaneix <nathalie.villa@univ-paris1.fr>
2013-08-16 19:05:33 +0200

    added the shiny interface to the package, while ignoring it for build

---
+++ inst/shiny/server.R
@@ -0,0 +1,91 @@
+library(NiLeDAM)
+
+shinyServer(function(input, output) {
+  tripletInput <- reactive({
+    in.file <- input$file1
+    
+    if (is.null(in.file))
+      return(NULL)
+    
+    if (input$rownames) {
+      read.table(in.file$datapath, header=input$header, sep=input$sep,
+               quote=input$quote, row.names=1)
+    } else {
+      read.table(in.file$datapath, header=input$header, sep=input$sep,
+                 quote=input$quote)
+    }
+  })
+  output$view <- renderTable({
+    head(tripletInput(), n=50)  
+  })
+
+	agesCalculation <- reactive({
+		the.triplets <- tripletInput()
+    if (input$boot==0)
+      return(NULL)
+		all.ages <- calculateAges(the.triplets, input$boot, input$risk/100,
+                              seed=input$seed) 
+	})
+  
+  agesDataSet <- reactive({
+    ages.object <- agesCalculation()
+    if (is.null(ages.object))
+      return(NULL)
+    ages.dataset <- cbind(ages.object@ages, ages.object@sd, t(ages.object@ci))
+    ages.dataset <- data.frame(ages.dataset)
+    names(ages.dataset) <- c("ages", "sd",
+                             paste("CI-",(ages.object@level/2*100),"%",sep=""),
+                             paste("CI-",(100-ages.object@level/2*100),"%",
+                                   sep=""))
+    ages.dataset
+  })
+  
+  output$ages <- renderTable({
+    agesDataSet()
+  })
+  
+  output$downloadAges <- downloadHandler(
+    filename = "ages.csv",
+    content = function(file) {
+      write.csv(agesDataSet(), file)
+  })
+
+	testsRes <- reactive({
+		the.ages <- agesCalculation()
+    nbmax <- input$nbmax
+    if (nbmax==0) {
+      the.tests <- tests(the.ages, nbmin=input$nbmin, level=input$level/100,
+                         verbose=FALSE)
+    } else {
+		  the.tests <- tests(the.ages, nbmin=input$nbmin, nbmax=input$nbmax,
+                         level=input$level/100, verbose=FALSE)
+    }
+	})
+  
+  output$testres <- renderPrint({
+    print(testsRes())
+  })
+  
+  prepareContent <- reactive({
+    test.res <- testsRes()
+    if (class(test.res)=="oneAgeTest") {
+      content <- test.res@which.pop
+    } else content <- test.res@best.res@which.pop
+    content
+  })
+  
+  output$downloadPops <- downloadHandler(
+    filename = "populations.csv",
+    content = function(file) {
+      write.table(prepareContent(), file, row.names=FALSE, col.names=FALSE)
+    }
+  )
+ 
+	output$densities <- renderPlot({
+		test.res <- testsRes()
+		plot(test.res,main=input$title,col=input$color)
+  })
+})
+
+
+
---
+++ inst/shiny/ui.R
@@ -0,0 +1,93 @@
+# Define UI for dataset viewer application
+shinyUI(pageWithSidebar(
+  # Application title
+  headerPanel("Welcome to the NiLeDAM online application !"),
+
+  sidebarPanel(
+		h2("Data importation"),
+    
+		fileInput('file1', 'Choose CSV/TXT File',
+		          accept=c('text/csv', 'text/comma-separated-values,text/plain')),
+		tags$hr(),
+		checkboxInput('header', ' Header?', TRUE),
+    checkboxInput('rownames', ' Row names?', FALSE),
+		radioButtons('sep', 'Separator:', c(Comma=',',Semicolon=';',Tab='\t'),
+                 'Comma'),
+		radioButtons('quote', 'Quote:',
+                 c(None='','Double Quote'='"','Single Quote'="'"),
+                 'Double Quote'),
+ 		br(),
+ 		h2("Age calculation"),
+ 		helpText(HTML("<strong>Note</strong>: When your dataset is properly
+                   imported, go to the 'Ages' panel and choose the number of
+                   bootstrap samples (increase it to 1000 for instance), run the
+                   calculation and go to the 'Ages' panel. <strong>You have to
+                   be patient</strong>, the computation time can be large.")),
+		h2("Tests"),
+    helpText(HTML("Once the ages calculated, the 'Test' panel is used to 
+                  determine from which number of age population, the analyses
+                  are coming."))
+  ),
+
+	mainPanel(
+	  tabsetPanel(
+	    tabPanel("Data",
+	             p(HTML("This application is a graphical interface of the <a
+                      href='http://niledam.r-forge.r-project.org/'>NiLeDAM</a>
+                      package using the <a href='http://cran.univ-paris1.fr/'>R
+                      </a> software environment.<br>If you have any trouble
+                      using it, please do not hesitate to contact <a 
+                      mailto:'nathalie[AT]nathalievilla.org'>Nathalie
+                      Villa-Vialaneix</a>, the package's maintainer.")),
+	             h3("Basic user guide"),
+	             p(HTML("To run the application, import your file with (U,Th,Pb)
+                      contents and corresponding errors. This file must be a
+                      text file having <strong>ONLY 6 columns</strong>: U, errU,
+                      Th, ErrTh, Pb, ErrPb, in this order (with eventually a
+                      first column containing the sample names). CSV format is
+                      the easiest to use: such an example is given <a 
+href='http://owncloud.nathalievilla.org/apps/files_sharing/get.php?token=51731049ed775fbe40746670171205596fff16f4'>
+                      here</a> that is imported with the default parameters
+                      specified on the left panel.<br>")),
+	             p("Once your file's URL is pasted, the data are imported and if
+                 the importation is done properly, the data are displayed on the
+                 main panel. Then, you can perform the age calculation and the
+                 tests by increasing the number of bootstrap samples."),
+	             h3("The dataset you want to use is displayed below (only the
+                  first 50 first lines if the dataset contains more than 50
+                  lines):"),
+	             tableOutput("view")
+	    ),
+      
+	    tabPanel("Ages",
+	             numericInput("boot", "Number of bootstrap samples (adviced:
+                            1000):", 0),
+	             numericInput("risk","Risk (%) for the confidence interval
+                            (adviced: 5%):", 5),
+	             numericInput("seed",
+                            "Set a random seed for reproducible results:",
+	                          as.numeric(format(Sys.time(), "%M%S"))),  
+               br(),br(),
+               downloadButton('downloadAges', 'Download Ages'),br(),br(),
+               tableOutput("ages")),
+      
+	    tabPanel("Tests",
+               helpText(HTML("Either supply 'nbmin' and 'nbmax' to find the
+                             most probable age population number or set 'nbmax'
+                             to 0 to perform a test for 'nbmin' age 
+                             populations.")),
+	             numericInput("nbmin", "Minimal number of populations tested:",1),
+	             numericInput("nbmax","Maximal number of populations tested",0),
+	             numericInput("level","Risk (%) for the confidence interval
+                            (adviced: 5%):", 5),
+               verbatimTextOutput("testres"),
+               downloadButton('downloadPops', 'Download population numbers'),
+               br(),br(),tableOutput("whichpop")),
+	    
+      tabPanel("Graphics",
+               textInput("title","Title","Ages"),
+               textInput("color","Color","red"),
+               plotOutput("densities"))
+	  )
+	)
+))
